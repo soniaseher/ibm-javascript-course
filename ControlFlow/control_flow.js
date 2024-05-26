ed355fe30829d4ce7c2171da6202eeba5e7c6920
soniaseher <sonia97811@gmail.com>
2024-05-26 02:48:40 -0400

    control flow practice

---
+++ ControlFlow/control_flow.js
@@ -0,0 +1,52 @@
+let userRole = "admin";
+let accessLevel;
+
+if (userRole === "admin") {
+    accessLevel = "Full access granted";
+} else if(userRole === "manager") {
+    accessLevel = "Limited access granted";
+} else {
+    accessLevel = "No access granted";
+}
+
+console.log("Access Level: ", accessLevel);
+
+let isLoggedIn = true;
+let userMessage;
+
+if(isLoggedIn) {
+    if(userRole === "admin") {
+        userMessage = "welcome, Admin";
+    } else {
+        userMessage = "Welcome, User!";
+    }
+} else {
+    userMessage = "Please login to access the system";
+}
+
+console.log("User Message: " , userMessage);
+
+// switch statement
+
+let userType = "subscriber";
+let userCategory;
+
+switch(userType) {
+    case "admin":
+        userCategory = "Administrator";
+        break;
+    case "manager":
+        userCategory = "Manager";
+        break;
+    case "subscriber" :
+        userCategory = "Subscriber";
+        break;
+    default:
+        userCategory = "Unknown";            
+}
+console.log("User Category:" , userCategory);
+
+let isAuthenticated = true;
+let authenticationStatus = isAuthenticated ? "Authenticated" : "Not authenticated";
+
+console.log("Authentication status:", authenticationStatus);
