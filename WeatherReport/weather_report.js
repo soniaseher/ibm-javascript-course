9c62718e71b38e8b8c93a5fbb4aec188b0cf7811
= <=>
2024-05-31 08:28:52 -0400

    weather app for fetch api practice

---
+++ WeatherReport/weather_report.js
@@ -0,0 +1,16 @@
+function showweatherDetails(event) {
+    event.preventDefault();
+}
+const city = document.getElementById('city').value;
+const apiKey = 'f2b50296fe8bb2f0079bd67466d710e4'; // Replace 'YOUR_API_KEY' with your actual API key
+const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+fetch(apiUrl)
+.then(response => response.json())
+.then(data => {
+  const weatherInfo = document.getElementById('weatherInfo');
+  weatherInfo.innerHTML = `<h2>Weather in ${data.name}</h2>
+                          <p>Temperature: ${data.main.temp} </p>
+                          <p>Weather: ${data.weather[0].description}</p>`;
+})
+
+       document.getElementById('weatherForm').addEventListener('submit',showweatherDetails );
